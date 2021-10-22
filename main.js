1d1a32f4fa6cab8d1332c272dec6cf88e6435de0
pierfrancescoscar <scarinci401@gmail.com>
2021-10-22 16:06:19 +0200

    add ref elements and open and closing buttons

---
+++ main.js
@@ -0,0 +1,17 @@
+// Mostrare e nascondere contenuto Hamburger Menu
+
+// Ref Elements
+
+// Open Button
+
+const hambMenuBtn = document.querySelector('a > .fa-bars');
+console.log(hambMenuBtn);
+
+// Closing Button
+
+const cloHambMenuBtn = document.querySelector('.close');
+console.log(cloHambMenuBtn);
+
+
+
+// Add click event
